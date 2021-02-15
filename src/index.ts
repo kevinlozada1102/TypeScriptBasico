730e0d6dc0971cfd63d4a99182dd05bf93873e90
kevinlozada1102 <kevinlozada1102@gmail.com>
2021-02-15 09:15:38 -0500

    introduccion a typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 
 
-console.log('Hola Mundo!');
+console.log('Hola Mundo KEvin!');
 
 /*
     ===== Código de TypeScript =====
 */
 
+let nombre: string = 'Kevin';
+
+// Declarar varios tipos a una variable
+let hp: number | string = 95;
+
+// Valores booleanos
+let estaVivo: boolean = true;
+
+hp = 'FULL';
+console.log(nombre, hp);
