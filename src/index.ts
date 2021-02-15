5abe0ae908092655f2db98e248f331eb973e5d97
kevinlozada1102 <kevinlozada1102@gmail.com>
2021-02-15 09:17:34 -0500

    separar en carpetas ejercicios

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
 
 
-console.log('Hola Mundo KEvin!');
+console.log('Hola Mundo!');
 
 /*
     ===== Código de TypeScript =====
 */
-
-let nombre: string = 'Kevin';
-
-// Declarar varios tipos a una variable
-let hp: number | string = 95;
-
-// Valores booleanos
-let estaVivo: boolean = true;
-
-hp = 'FULL';
-console.log(nombre, hp);
